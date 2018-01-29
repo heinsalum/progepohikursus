f256fb315f5d1e22dae57b438ee14264ede41da1
Markus Heinsalu <heinsalum@users.noreply.github.com>
2018-01-29 09:20:15 +0200

    Esimene tund

--- tund1/Tervitus.java
+++ tund1/Tervitus.java
@@ -1,5 +1,17 @@
 public class Tervitus{
 	public static void main(String[] arg){
 		System.out.println("Tere");
+		int temperatuur=55;
+		if(temperatuur<-28){
+			System.out.println("Külmapüha");
+		} else {
+			//kui õues on soojem kui 20 kraadi
+			//siis soovita lühikesi varrukaid
+			if(temperatuur>20){
+				System.out.println("Pane T-särk selga");
+			} else {
+				System.out.println("Võta jope kaasa");
+			}
+		}
 	}
 }
